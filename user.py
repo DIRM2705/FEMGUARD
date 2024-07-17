09d26608de78c554fd40e9f0198504365fd92a78
DIRM2705 <dirm2705@gmail.com>
2024-07-17 13:09:21 -0600

    Añade clase con datos del usuario

---
+++ user.py
@@ -0,0 +1,11 @@
+class UserData:
+    def __init__(self, name : str, age : int, phone_number : str, blood_type : str, height : float, weight : float, allergies : list[str], emergency_contacts : list[str]):
+        self.name = name
+        self.age = age
+        self.phone_number = phone_number
+        self.blood_type = blood_type
+        self.height = height
+        self.weigth = weight
+        self.allergies = allergies
+        self.emergency_contacts = emergency_contacts
+        
