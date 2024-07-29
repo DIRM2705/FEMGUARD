4a25de75279905cc8f61c6671a91bcd181efa2dd
DIRM2705 <dirm2705@gmail.com>
2024-07-29 12:14:43 -0600

    Une Frontend a main.py

--- Frontend/logo_screen.py
+++ Frontend/logo_screen.py
@@ -1,24 +1,21 @@
 import flet as ft
 def setup_screen(page : ft.Page):
     page.window.maximizable = False
-    page.window.width = 300 #Ancho de la pantalla tani cambialo a 395  350
-    page.window.height = 500 #Largo de la pantalla   750
+    page.window.width = 350 #Ancho de la pantalla tani cambialo a 395  350
+    page.window.height = 750 #Largo de la pantalla   750
     page.bgcolor = "White"
+    page.padding=0
+    page.adaptative= True
 
-def add_bars(page : ft.Page):
+def add_appbar(page : ft.Page):
     page.appbar = ft.AppBar(
         bgcolor="#E1BFD0", #Color de la barra superior
         toolbar_height=35, #Altura de la barra superior
         shape=ft.RoundedRectangleBorder(radius=3.5)) #Cambiar radio para cambiar cuanto se redondea la barra superior
 
 def main(page: ft.Page):
-    page.bgcolor = "white"
-    page.padding=0
-    page.adaptative= True
-
-
     img = ft.Image(
-        src=f"C:/Users/whyga/Downloads/Image (1).png",
+        src=f"images/FEMGUARD LOGO.png",
         width=225,
         height=225,
         fit=ft.ImageFit.CONTAIN,
@@ -28,6 +25,5 @@ def main(page: ft.Page):
     page.horizontal_alignment=ft.CrossAxisAlignment.CENTER 
 
     page.add(img)
-    add_bars(page)
+    add_appbar(page)
     page.update()
-ft.app(target=main)
---
+++ main.py
@@ -0,0 +1,5 @@
+from Frontend.logo_screen import main
+import flet as ft
+import asyncio
+
+ft.app(target=main, name="FEMGUARD")
