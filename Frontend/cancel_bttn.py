f5c09c18054159e3978944ca2923ca280e3cc8a9
DIRM2705 <dirm2705@gmail.com>
2024-07-29 21:39:53 -0600

    Añade pantalla botón cancelar

---
+++ Frontend/cancel_bttn.py
@@ -0,0 +1,31 @@
+import flet as ft
+from bleak import BleakGATTCharacteristic
+import asyncio
+
+def main(page : ft.Page):
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    cancel_bttn = ft.ElevatedButton(
+        text="DETENER ALARMA", 
+        style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=25)),
+        bgcolor="Red"
+    )
+    
+    page.add(cancel_bttn)
+    
+async def panic_function(sender : BleakGATTCharacteristic, data : bytearray):
+    '''
+    Función que se debe llamar cuando al recibir la alerta inmediata del collar indicando que hay una emergencia.
+    Espera 15 segundos antes de cerrar la pantalla del botón cancelar y 
+    comenzar a realizar las acciones de seguridad
+    '''
+    HIGH_ALERT_MSSG = "High Alert"
+        
+    if data.decode() == HIGH_ALERT_MSSG:    
+        try:
+            task = asyncio.create_task()
+            await asyncio.wait_for(task, timeout=15)
+        except TimeoutError:
+            #No se presionó el botón dentro de los 15 segundos
+            print("La grabación comenzó")
+            print("Notificar contactos")
